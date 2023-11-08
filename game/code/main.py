97941789aff4a5acedfba445a68d912394d8a146
Johan-Greyling-Cauchy <jo.greyling@gmail.com>
2023-11-08 18:15:55 +0200

    ok can finally get started.... added custom screen caption

--- game/code/main.py
+++ game/code/main.py
@@ -4,16 +4,17 @@ from level import Level
 
 class Game:
 	def __init__(self):
-		  
+
 		# general setup
 		pygame.init()
 		self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
-		pygame.display.set_caption('Zelda')
+		pygame.display.set_caption('I customized this title myself')
 		self.clock = pygame.time.Clock()
 
 		self.level = Level()
-	
+
 	def run(self):
+
 		while True:
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
